b350898661b57a21270408dbd90d80f028ea28f7
klaar <klaar@cohen.(none)>
2009-05-24 22:15:04 +0200

    Added shrinking of transformed values

--- src/rapperl_map.erl
+++ src/rapperl_map.erl
@@ -1,7 +1,21 @@
 -module(rapperl_map, [Gen, Fun]).
 
--export([value/0]).
+-export([value/0
+        ,shrink_strategies/0
+        ,shrinks_with/2
+        ,shrink/2]).
 
 value() ->
    Val = rapperl:pop(Gen),
    {Val, Fun(Val)}.
+
+shrink_strategies() ->
+   Gen:shrink_strategies().
+
+shrinks_with(Strategy, {OrgVal, _}) ->
+   Gen:shrinks_with(Strategy, OrgVal).
+
+shrink({OrgVal, _}, Strategy) ->
+   NewVal = Gen:shrink(OrgVal, Strategy),
+   {NewVal, Fun(NewVal)}.
+   
