7feb98c5210107133594d170a9aa6cecf8548a8e
klaar <klaar@cohen.(none)>
2009-05-24 22:30:49 +0200

    Added shrinking of filtered values

--- src/rapperl_filter.erl
+++ src/rapperl_filter.erl
@@ -1,6 +1,9 @@
 -module(rapperl_filter, [Gen, Pred]).
 -include("rapperl_constants.hrl").
--export([value/0]).
+-export([value/0
+        ,shrink_strategies/0
+        ,shrinks_with/2
+        ,shrink/2]).
 
 value() ->
    Val = rapperl:pop(Gen),
@@ -13,3 +16,18 @@ value(0, _, _) ->
 value(N, false, _) ->
    Val = rapperl:pop(Gen),
    value(N - 1, Pred(Val), Val).
+
+shrink_strategies() ->
+   Gen:shrink_strategies().
+
+shrinks_with(Strategy, Value) ->   
+   Gen:shrinks_with(Strategy, Value).
+
+shrink(Value, Strategy) ->
+   NewVal = Gen:shrink(Value, Strategy),
+   case Pred(NewVal) of
+      true ->
+         NewVal;
+      false ->
+         Value
+   end.
