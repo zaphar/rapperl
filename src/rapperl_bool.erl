90cc6af59463c78874bccd5beb131b7ee336ada0
klaar <klaar@cohen.(none)>
2009-05-24 23:14:49 +0200

    added shrinking of booleans values down to false

--- src/rapperl_bool.erl
+++ src/rapperl_bool.erl
@@ -1,6 +1,9 @@
 -module(rapperl_bool, [Val, In, OutOf]).
 
--export([value/0]).
+-export([value/0
+        ,shrink_strategies/0
+        ,shrinks_with/2
+        ,shrink/2]).
 
 value() when Val == true ->
    Prob = rapperl:pop(rapperl:int(1, OutOf)),
@@ -8,3 +11,16 @@ value() when Val == true ->
 value() when Val == false ->
    Prob = rapperl:pop(rapperl:int(1, OutOf)),
    not (Prob =< In).
+
+% Booleans shrink to false
+
+shrink_strategies() ->
+   [make_false].
+
+shrinks_with(make_false, true) ->
+   true;
+shrinks_with(make_false, false) ->
+   false.
+
+shrink(true, make_false) ->
+   false.
